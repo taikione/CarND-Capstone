1ae78739ae2ad281d7c6302160ffbfece0e1538b
taikione <taiki.one4k@gmail.com>
2017-12-09 20:43:52 +0900

    Add scripts to train tl classification model

---
+++ ros/src/tl_detector/light_classification/train_model/generator.py
@@ -0,0 +1,61 @@
+import cv2
+import sklearn.utils
+import numpy as np
+
+
+class BatchGenerator:
+
+    def __init__(self, image_paths, labels, batch_size):
+
+        self.image_paths = image_paths
+        self.labels = labels
+        self.batch_size = batch_size
+        self.width = 224
+        self.height = 224
+
+        self.image_batch = []
+        self.label_batch = []
+
+        self.labels_dict = {"red":0, "yellow":1, "green":2, "unknown":3}
+
+    def get_one_hot_label(self, label):
+
+        one_hot = np.zeros(len(self.labels_dict))
+        one_hot[self.labels_dict[label]] = 1
+
+        return one_hot
+
+    def get_images_count(self):
+
+        return len(self.image_paths)
+
+    def get_labels_count(self):
+
+        return len(self.labels)
+
+    def get_batch_generator(self):
+
+        sklearn.utils.shuffle(self.image_paths, self.labels)
+
+        for image_path, label in zip(self.image_paths, self.labels):
+
+            image = cv2.imread(image_path)
+            one_hot_label = self.get_one_hot_label(label)
+
+            if not(image.shape[0] == self.height and image.shape[1] == self.width):
+
+                image = cv2.resize(image, (self.height, self.width))
+
+            self.image_batch.append(image)
+            self.label_batch.append(one_hot_label)
+
+            if (len(self.image_batch) is self.batch_size) and (len(self.label_batch) is self.batch_size):
+
+                yield (sklearn.utils.shuffle(np.array(self.image_batch), np.array(self.label_batch)))
+
+                self.image_batch.clear()
+                self.label_batch.clear()
+
+
+
+
---
+++ ros/src/tl_detector/light_classification/train_model/train_model.py
@@ -0,0 +1,118 @@
+import os
+import cv2
+import json
+import numpy as np
+import tensorflow.contrib.keras as keras
+# from tensorflow.contrib.keras import backend as K
+
+import generator
+
+
+def load_json(path):
+
+    with open(path) as file:
+
+        json_file = json.load(file)
+
+    return json_file
+
+
+def get_one_hot_label(label):
+
+    labels_dict = {"red":0, "yellow":1, "green":2, "unknown":3}
+
+    one_hot = np.zeros(len(labels_dict))
+    one_hot[labels_dict[label]] = 1
+
+    return one_hot
+
+def get_callbacks(model_path):
+
+    os.makedirs(os.path.dirname(model_path), exist_ok=True)
+    model_checkpoint = keras.callbacks.ModelCheckpoint(filepath=model_path, save_best_only=True, verbose=1)
+
+    reduce_learning_rate_callback = keras.callbacks.ReduceLROnPlateau(factor=0.5, patience=2, verbose=1)
+    early_stop_callback = keras.callbacks.EarlyStopping(patience=5, verbose=1)
+
+    return [model_checkpoint, reduce_learning_rate_callback, early_stop_callback]
+
+
+def get_train_model():
+
+    img_input = keras.layers.Input(shape=(224, 224, 3), name='vgg_input', dtype='float32')
+
+    vgg16_model = keras.applications.VGG16(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+    vgg16_model.trainable = False
+
+    vgg16_output = vgg16_model(img_input)
+    x = keras.layers.Flatten()(vgg16_output)
+    x = keras.layers.Dense(1024, activation='elu')(x)
+    preds = keras.layers.Dense(4, activation='softmax')(x)
+
+    model = keras.models.Model(inputs=img_input, outputs=preds)
+    adam = keras.optimizers.Adam(lr=0.0001)
+    model.compile(optimizer=adam, loss="binary_crossentropy")
+
+    return model
+
+
+def main():
+
+    # path to dataset
+    data_directory = "path/to/tl_images_dataset"
+
+    # path to save model
+    tl_model_path = os.path.join(data_directory, "tl_model.h5")
+
+    # load json files representing image and label paths
+    train_image_json_path = os.path.join(data_directory, "json", "train_data.json")
+    train_label_json_path = os.path.join(data_directory, "json", "train_label.json")
+
+    valid_image_json_path = os.path.join(data_directory, "json", "valid_data.json")
+    valid_label_json_path = os.path.join(data_directory, "json", "valid_label.json")
+
+    train_image_paths = load_json(train_image_json_path)
+    valid_image_paths = load_json(valid_image_json_path)
+
+    # get full path to each image
+    train_image_paths = [os.path.join(data_directory, p) for p in train_image_paths]
+    valid_image_paths = [os.path.join(data_directory, p) for p in valid_image_paths]
+
+    train_labels = load_json(train_label_json_path)
+    valid_labels = load_json(valid_label_json_path)
+
+    batch_size = 4
+
+    train_batch_generator = generator.BatchGenerator(train_image_paths, train_labels, batch_size)
+    valid_batch_generator = generator.BatchGenerator(valid_image_paths, valid_labels, batch_size)
+
+    # get model
+    model = get_train_model()
+
+    # train
+    model.fit_generator(
+        train_batch_generator.get_batch_generator(),
+        steps_per_epoch=train_batch_generator.get_images_count() / batch_size,
+        epochs=1,
+        validation_data=valid_batch_generator.get_batch_generator(),
+        validation_steps=valid_batch_generator.get_images_count() / batch_size,
+        callbacks=get_callbacks(tl_model_path)
+    )
+
+    # test
+    test_image_json_path = os.path.join(data_directory, "json", "test_data.json")
+    test_label_json_path = os.path.join(data_directory, "json", "test_label.json")
+
+    test_image_paths = load_json(test_image_json_path)
+    test_labels = load_json(test_label_json_path)
+
+    test_image_paths = [os.path.join(data_directory, p) for p in test_image_paths]
+    test_images = np.array([cv2.imread(path) for path in test_image_paths])
+    test_one_hot_labels = np.array([get_one_hot_label(label) for label in test_labels])
+
+    loss, accuracy = model.evaluate(test_images, test_one_hot_labels, verbose=0)
+    print("Accuracy = {:.2f}".format(accuracy))
+
+
+if __name__ == "__main__":
+    main()
